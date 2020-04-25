3aae312eb04d31bbffb31d0a53d67fa4ac686bb0
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-25 12:27:50 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -1,14 +1,10 @@
-require_relative './spec_helper'
-require 'benchmark'
-
-describe 'LOOPS' do
-
-  describe '#countdown' do
-    let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
-
-    it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
-      expect { countdown(10) }.to output(countdown_output).to_stdout
-    end
+def countdown(int)
+  
+  while int > 0 
+   puts "#{int} SECOND(S)!"
+   int -=
+  end
+  
 
     it 'returns HAPPY NEW YEAR!' do
       expect(countdown(12)).to eq "HAPPY NEW YEAR!"

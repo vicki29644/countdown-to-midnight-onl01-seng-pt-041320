60df04bd066ec9ec76d6ef27e13ef9cdc8a6b751
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-25 12:38:14 +0000

    Automatically backed up by Learn

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -8,14 +8,5 @@ def countdown(int)
 end
 
 
-    end
-  end
+    
 
-  describe '#countdown_with_sleep' do
-    it 'should take at least 5 seconds to execute' do
-      runtime = Benchmark.measure { countdown_with_sleep(5) }
-      expect(runtime.real.to_i >= 5).to be true
-    end
-  end
-
-end
